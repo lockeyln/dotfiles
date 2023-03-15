c78f54c27a797b7a7ea76810d309e5d92cb9dd26
lockeyln <2815657324@qq.com>
2023-03-15 22:54:42 +0800

    修改wezterm配置

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,216 +1,106 @@
 local wezterm = require("wezterm")
+local custom = require("colors.custom")
+local keybindings = require("config.key-bindings")
+local launch_menu = require("config.launch-menu")
+local ssh_domains = require("config.ssh-domains")
+local shell = require("config.shell")
+require("config.right-status").setup()
+require("config.notify").setup()
+require("config.tab-title").setup()
 
-function font_with_fallback(name, params)
-    local names = { name, "FiraCode Nerd Font Mono" }
+local font_primary = "FiraCode Nerd Font Mono"
 
-    return wezterm.font_with_fallback(names, params)
+local function font(name, params)
+   return wezterm.font(name, params)
 end
 
-local config = {
-    check_for_updates = false,
-    -- color_scheme = "Fahrenheit",
-    -- color_scheme = "Gruvbox Dark",
-    -- color_scheme = "Blue Matrix",
-    -- color_scheme = "Pandora",
-    -- color_scheme = "Grape",
-    -- color_scheme = "Firewatch",
-    -- color_scheme = "Duotone Dark",
-    -- color_scheme = "Sakura",
-    -- color_scheme = "lovelace",
-    -- color_scheme = "lovelace",
-    color_scheme = "Whimsy",
+-- [[ font with fallback ]]
+-- local function font_with_fallback(name, params)
+--   local names = { name }
+--   return wezterm.font_with_fallback(names, params)
+-- end
 
-    enable_scroll_bar = true,
-    exit_behavior = "Close",
+return {
+   -- fonts
+   font = font(font_primary),
+   font_size = 13,
 
-    -- tab_bar_at_bottom = true,
-    inactive_pane_hsb = {
-        hue = 1.0,
-        saturation = 1.0,
-        brightness = 1.0,
-    },
+   -- colour scheme
+   colors = custom,
 
-    font = font_with_fallback("JetBrains Mono"),
-    font_antialias = "Subpixel",
-    font_dirs = { "fonts" },
-    font_rules = {
-        {
-            intensity = "Bold",
-            font = font_with_fallback("JetBrains Mono Bold"),
-        },
-    },
-    font_size = 13.0,
+   -- background
+   background = {
+      {
+         source = { File = wezterm.config_dir .. "/backdrops/space.jpg" },
+      },
+      {
+         source = { Color = custom.background },
+         height = "100%",
+         width = "100%",
+         opacity = 0.90,
+      },
+   },
 
-    -- Tab bar appearance
-    colors = {
-        tab_bar = {
+   -- scroll bar
+   enable_scroll_bar = true,
 
-            -- The color of the strip that goes along the top of the window
-            background = "#282828",
+   -- status
+   status_update_interval = 1000,
 
-            -- The active tab is the one that has focus in the window
-            active_tab = {
-                -- The color of the background area for the tab
-                bg_color = "#18131e",
-                -- The color of the text for the tab
-                fg_color = "#ff65fd",
+   -- tab bar
+   enable_tab_bar = true,
+   hide_tab_bar_if_only_one_tab = false,
+   use_fancy_tab_bar = false,
+   tab_max_width = 25,
+   show_tab_index_in_tab_bar = false,
+   switch_to_last_active_tab_when_closing_tab = true,
 
-                intensity = "Normal",
-                underline = "None",
-                italic = false,
-                strikethrough = false,
-            },
+   -- window
+   window_padding = {
+      left = 5,
+      right = 10,
+      top = 12,
+      bottom = 7,
+   },
+   window_close_confirmation = "NeverPrompt",
+   window_frame = {
+      active_titlebar_bg = "#090909",
+      font = font(font_primary, { bold = true }),
+      font_size = 9,
+   },
+   automatically_reload_config = true,
+   inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
+   -- window_background_opacity = 0.9,
 
-            -- Inactive tabs are the tabs that do not have focus
-            inactive_tab = {
-                bg_color = "#282828",
-                fg_color = "#d19afc",
-            },
-            inactive_tab_hover = {
-                bg_color = "#202020",
-                fg_color = "#ff65fd",
-            },
+   -- keybindings
+   disable_default_key_bindings = true,
+   keys = keybindings,
 
-            new_tab = {
-                bg_color = "#282828",
-                fg_color = "#d19afc",
-            },
-            new_tab_hover = {
-                bg_color = "#18131e",
-                fg_color = "#ff65fd",
-            },
-        },
-    },
+   -- mousebindings
+   mouse_bindings = {
+      -- Ctrl-click will open the link under the mouse cursor
+      {
+         event = { Up = { streak = 1, button = "Left" } },
+         mods = "CTRL",
+         action = wezterm.action.OpenLinkAtMouseCursor,
+      },
+   },
 
-    keys = {
-        {
-            key = "Insert",
-            mods = "SHIFT",
-            action = "Paste",
-        },
-        {
-            key = "c",
-            mods = "CTRL | SHIFT",
-            action = "Copy",
-        },
-        {
-            key = "v",
-            mods = "CTRL | SHIFT",
-            action = "Paste",
-        },
-        {
-            key = "x",
-            mods = "CTRL | SHIFT",
-            action = "ActivateCopyMode",
-        },
-        {
-            key = "z",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ EmitEvent = "plumb" }),
-        },
-        {
-            key = "PageUp",
-            mods = "SHIFT",
-            action = wezterm.action({ ScrollByPage = -1 }),
-        },
-        {
-            key = "PageDown",
-            mods = "SHIFT",
-            action = wezterm.action({ ScrollByPage = 1 }),
-        },
-        {
-            key = "+",
-            mods = "CTRL | SHIFT",
-            action = "IncreaseFontSize",
-        },
-        {
-            key = "-",
-            mods = "CTRL | SHIFT",
-            action = "DecreaseFontSize",
-        },
-        {
-            key = "t",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-        },
-        {
-            key = "w",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ CloseCurrentTab = { confirm = true } }),
-        },
-        {
-            key = "LeftArrow",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ ActivateTabRelative = -1 }),
-        },
-        {
-            key = "RightArrow",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ ActivateTabRelative = 1 }),
-        },
-        {
-            key = "PageUp",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ MoveTabRelative = -1 }),
-        },
-        {
-            key = "PageDown",
-            mods = "CTRL | SHIFT",
-            action = wezterm.action({ MoveTabRelative = 1 }),
-        },
-    },
-    mouse_bindings = {
-        {
-            event = {
-                Down = {
-                    streak = 1,
-                    button = "Left",
-                },
-            },
-            mods = "NONE",
-            action = wezterm.action({ SelectTextAtMouseCursor = "Cell" }),
-        },
-        {
-            event = {
-                Down = {
-                    streak = 2,
-                    button = "Left",
-                },
-            },
-            mods = "NONE",
-            action = wezterm.action({ SelectTextAtMouseCursor = "Word" }),
-        },
-        {
-            event = {
-                Down = {
-                    streak = 3,
-                    button = "Left",
-                },
-            },
-            mods = "NONE",
-            action = wezterm.action({ SelectTextAtMouseCursor = "Line" }),
-        },
-        {
-            event = {
-                Up = {
-                    streak = 1,
-                    button = "Left",
-                },
-            },
-            mods = "NONE",
-            action = wezterm.action({ CompleteSelectionOrOpenLinkAtMouseCursor = "PrimarySelection" }),
-        },
-        {
-            event = {
-                Down = {
-                    streak = 1,
-                    button = "Middle",
-                },
-            },
-            mods = "NONE",
-            action = "Paste",
-        },
-    },
+   -- shells
+   default_prog = shell,
+   launch_menu = launch_menu,
+
+   -- ssh
+   ssh_domains = ssh_domains,
+
+   -- wsl
+   wsl_domains = {
+      {
+         name = "WSL:Ubuntu",
+         distribution = "Ubuntu",
+         username = "lockeyln",
+         default_cwd = "/home/lockeyln",
+         default_prog = { "zsh" },
+      },
+   },
 }
-
-return config
